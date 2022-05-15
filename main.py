189ddbc0fe365cde86083e969cc136bdf7f8668f
Frederic-Tang <1206277595@qq.com>
2022-05-16 04:08:29 +0800

    加入计时

--- main.py
+++ main.py
@@ -12,6 +12,32 @@ stop= pygame.image.load('暂停.png')
 ballrect = ball.get_rect()  # 获取矩形区域
 speed = [5, 5]  # 设置移动的X轴、Y轴
 clock = pygame.time.Clock()  # 设置时钟
+black = (0, 0, 0)
+white = (255, 255, 255)
+display_width = 800
+display_height = 600
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+clock = pygame.time.Clock()
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    # 在一个新 Surface 对象上绘制文本
+    return textSurface, textSurface.get_rect()
+def game_clock():
+    clock_ = 300
+    while True:
+        pygame.time.get_ticks()
+        if (pygame.time.get_ticks()%1000 == 0):
+            clock_ = clock_ - 1
+            clock_g = str(clock_)
+            gameDisplay.fill(white)
+            largeText = pygame.font.SysFont('comicsansms', 25)
+            TextSurf, TextRect = text_objects(clock_g, largeText)
+            TextRect.center = (750, (display_height / 10))
+            gameDisplay.blit(TextSurf, TextRect)
+            pygame.display.update()
+            clock.tick(15)
+            print(clock_)
+game_clock()
 
 while True:  # 死循环确保窗口一直显示
     clock.tick(60)  # 每秒执行60次
